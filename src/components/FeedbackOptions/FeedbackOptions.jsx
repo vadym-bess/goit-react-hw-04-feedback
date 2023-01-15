c24f69d55300cc6141ef4892bdd11b634c7aefea
Vadym <besarabvadim2@gmail.com>
2023-01-15 16:10:41 +0200

    Update FeedbackOptions.jsx

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -23,6 +23,6 @@ export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
 };
 
 FeedbackOptions.propTypes = {
-      options: PropTypes.arrayOf(PropTypes.string).isRequired,
-      onLeaveFeedback: PropTypes.func.isRequired
-}
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
